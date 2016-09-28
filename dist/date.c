ed3b251892f5b9a8f94fd14f1befa8b15ddc0366
Evan French <frenchev@pdx.edu>
2016-09-28 13:51:41 -0700

    date.c started

---
+++ dist/date.c
@@ -0,0 +1,15 @@
+#include "types.h" 
+#include "user.h"
+#include "date.h"
+
+
+int main (int argc, char* argv[]){
+{
+    
+    //contains all the pieces of time
+    struct rtcdate r;
+    
+
+    if (date(&r){
+
+
