5fecaece55a98a8afd6f61da97ea4aa3bf881624
Evan French <frenchev@pdx.edu>
2016-09-28 22:08:31 -0700

    date.c updated

--- dist/date.c
+++ dist/date.c
@@ -7,9 +7,27 @@ int main (int argc, char* argv[]){
 {
     
     //contains all the pieces of time
+    //with resolution of one second
     struct rtcdate r;
-    
+    r  = cmostime(&r);  //fill r
 
-    if (date(&r){
+    if (date(&r)) {
 
+        printf(2, "Date_failed\n");
+        exit();
+    }
+
+    //date exists
+    cprintf(" day: %d /
+              month: %d /
+              year: %d \t 
+              hour: %d
+              minute: %d 
+              second: %d \n",
+              r.day, r.month, r.year, r.hour, r.minute, r.second);
+
+
+    exit();
+}
+        
 
